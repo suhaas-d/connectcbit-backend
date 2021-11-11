b5d9d25029c635df68f179ce7b5087aa1141149a
suhaas-d <suhaasd13@gmail.com>
2021-11-11 22:58:35 +0530

    Made changes for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn blog_backend.wsgi --log-file -
+web: gunicorn locallibrary.wsgi --log-file -
--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -141,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 REACT_APP_DIR = os.path.join(BASE_DIR, 'blog_frontend') 
 
@@ -163,3 +164,17 @@ REST_FRAMEWORK = {
     ),
 
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
