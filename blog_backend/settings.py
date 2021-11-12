8fd3ad028d3c71571b2769347677d1bf8eac00bd
suhaas-d <suhaasd13@gmail.com>
2021-11-12 10:20:03 +0530

    allowed hosts

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG =True
 
-ALLOWED_HOSTS = ['connect-cbit.herokuapp.com/']
+ALLOWED_HOSTS = ['https://murmuring-bayou-16041.herokuapp.com/']
 
 
 # Application definition
