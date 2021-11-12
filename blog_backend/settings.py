9230876ad8266f00094c55a3455853740101773d
suhaas-d <suhaasd13@gmail.com>
2021-11-12 11:23:34 +0530

    allowed hosts changed

--- blog_backend/settings.py
+++ blog_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG =True
 
-ALLOWED_HOSTS = ['https://murmuring-bayou-16041.herokuapp.com/']
+ALLOWED_HOSTS = ['murmuring-bayou-16041.herokuapp.com']
 
 
 # Application definition
